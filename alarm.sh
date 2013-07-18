4919174c1baa60574d58ea06e28026e3f1641980
woonohyo <wonhyo.yi@gmail.com>
2013-07-18 13:51:37 +0900

    Alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -15,38 +15,4 @@ do
 	echo 
 	echo "Press Ctrl+C to stop"
 	sleep 0.1
-done#!bin/bash
-read timer
-
-while [ $timer -gt 0 ]
-do
-	echo 
-	echo $timer
-	timer=$((timer-1))
-	sleep 1
-done
-
-echo "Times Up!"
-while [ $timer -eq 0 ]
-do
-	echo 
-	echo "Press Ctrl+C to stop"
-	sleep 0.1
-done#!bin/bash
-read timer
-
-while [ $timer -gt 0 ]
-do
-	echo 
-	echo $timer
-	timer=$((timer-1))
-	sleep 1
-done
-
-echo "Times Up!"
-while [ $timer -eq 0 ]
-do
-	echo 
-	echo "Press Ctrl+C to stop"
-	sleep 0.1
 done
